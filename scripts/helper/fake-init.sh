0bebbecfc99f062601122e849bc2aa4b30cd41b2
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-09 12:26:11 -0400

    test

--- scripts/helper/fake-init.sh
+++ scripts/helper/fake-init.sh
@@ -2,9 +2,12 @@
 
 export PS1='\u in \W: '
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 
 for file in ../../bootstrap/init/*.yaml; do
   echo "Applying $file"
   kubectl apply -f "$file"
 done
+
+# kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.7.2/manifests/install.yaml
+# kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d
+
