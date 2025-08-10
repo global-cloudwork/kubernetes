2ce4d4c39a4681817c055a25f9c662f8e6d281c0
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-10 12:42:49 -0400

    test

--- scripts/remove-and-init.sh
+++ scripts/remove-and-init.sh
@@ -7,8 +7,6 @@ sudo chmod 644 /etc/rancher/k3s/k3s.yaml
 kubectl kustomize --enable-helm \
   "github.com/global-cloudwork/kubernetes?ref=development" \
   | kubectl apply --dry-run=client -f -
-  
+
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d
 kubectl port-forward svc/argocd-server -n argocd 8080:443
-
-
