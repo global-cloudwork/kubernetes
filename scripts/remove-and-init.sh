db4b3bfc52774af753b3c201785a3ab2c7d8c3eb
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-14 12:29:51 -0400

    test

--- scripts/remove-and-init.sh
+++ scripts/remove-and-init.sh
@@ -4,10 +4,11 @@
 
 curl -sfL https://get.rke2.io | sudo sh -
 
-mkdir -p /etc/rancher/rke2/server/manifests/
+mkdir -p /etc/rancher/rke2/
+mkdir -p /var/lib/rancher/rke2/server/manifests/
 
 cp ../configurations/etc-rancher-rke2-config.yaml /etc/rancher/rke2/config.yaml
-cp ../applications/core/cilium/helm-chart-config.crd.yaml /etc/rancher/rke2/server/manifests/rke2-cilium-config.yaml
+cp ../applications/core/cilium/helm-chart-config.crd.yaml /var/lib/rancher/rke2/server/manifests/rke2-cilium-config.yaml
 
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
