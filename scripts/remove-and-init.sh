f21fa607d9fe1f41219a024d6bd349af341599b4
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-13 23:45:41 -0400

    test

--- scripts/remove-and-init.sh
+++ scripts/remove-and-init.sh
@@ -2,11 +2,11 @@
 # Run as root
 /usr/local/bin/rke2-uninstall.sh
 
-mkdir -p /etc/rancher/rke2/
-
-cp ../configurations/etc-rancher-rke2-config.yaml /etc/rancher/rke2/config.yaml
 
 curl -sfL https://get.rke2.io | sudo sh -
+mkdir -p /etc/rancher/rke2/
+cp ../configurations/etc-rancher-rke2-config.yaml /etc/rancher/rke2/config.yaml
+
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
 
