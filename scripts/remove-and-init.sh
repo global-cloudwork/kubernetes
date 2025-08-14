0deb73cec7dd5f74141ac9134e6900568c3ea4a1
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-14 00:15:21 -0400

    test

--- scripts/remove-and-init.sh
+++ scripts/remove-and-init.sh
@@ -13,26 +13,22 @@ systemctl start rke2-server.service
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
 chmod 644 /etc/rancher/rke2/rke2.yaml
 
-kubectl create namespace argocd
-kubectl create namespace cert-manager
+# kubectl create namespace argocd
+# kubectl create namespace cert-manager
 
-kubectl apply -k https://github.com/kubernetes-sigs/gateway-api/config/crd
-kubectl apply -f ../applications/core/cilium/helm-chart-config.crd.yaml
+# kubectl apply -k https://github.com/kubernetes-sigs/gateway-api/config/crd
+# kubectl apply -f ../applications/core/cilium/helm-chart-config.crd.yaml
 
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
-  | kubectl apply -f -
+# kubectl kustomize --enable-helm \
+#   "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
+#   | kubectl apply -f -
 
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes/applications/core/cert-manager?ref=development" \
-  | kubectl apply -f -
+# kubectl kustomize --enable-helm \
+#   "github.com/global-cloudwork/kubernetes/applications/core/cert-manager?ref=development" \
+#   | kubectl apply -f -
 
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes?ref=development" \
-  | kubectl apply -f -
+# kubectl kustomize --enable-helm \
+#   "github.com/global-cloudwork/kubernetes?ref=development" \
+#   | kubectl apply -f -
 
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes?ref=development" \
-  | kubectl apply -f -
-
-kubectl create secret tls ca -n argocd --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
+# kubectl create secret tls ca -n argocd --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
