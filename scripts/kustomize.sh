d3ce7d60daa9c27832b9cf9efd0411add187bfa2
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-14 11:13:32 -0400

    test

--- scripts/kustomize.sh
+++ scripts/kustomize.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-kubectl create secret tls argocd-server-tls -n argocd --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
+kubectl create secret tls ca -n argocd --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
 
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
 chmod 644 /etc/rancher/rke2/rke2.yaml
@@ -9,7 +9,6 @@ kubectl create namespace cert-manager
 
 # kubectl apply -k https://github.com/kubernetes-sigs/gateway-api/config/crd
 kubectl apply -f "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml"
-
 
 kubectl kustomize --enable-helm \
   "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
@@ -23,4 +22,3 @@ kubectl kustomize --enable-helm \
   "github.com/global-cloudwork/kubernetes?ref=development" \
   | kubectl apply -f -
 
-kubectl create secret tls ca -n argocd --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
