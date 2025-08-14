d45a0f9c58a70775c0029c58a61c9e417ed3c767
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-14 13:11:40 -0400

    test

--- scripts/remove-and-init.sh
+++ scripts/remove-and-init.sh
@@ -17,18 +17,3 @@ systemctl start rke2-server.service
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
 chmod 644 /etc/rancher/rke2/rke2.yaml
 
-kubectl create namespace argocd
-kubectl create namespace cert-manager
-# kubectl create secret tls ca -n argocd --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
-
-# kubectl apply -k https://github.com/kubernetes-sigs/gateway-api/config/crd
-kubectl apply -f "https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.0.0/standard-install.yaml"
-
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
-  | kubectl apply -f -
-
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes/applications/core/cert-manager?ref=development" \
-  | kubectl apply -f -
-
