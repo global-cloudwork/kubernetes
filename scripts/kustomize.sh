fc62aeb986ea7dc18b2ff93fd2e7f5015f132275
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-13 22:51:06 -0400

    test

--- scripts/kustomize.sh
+++ scripts/kustomize.sh
@@ -9,7 +9,6 @@ kubectl kustomize --enable-helm \
   | kubectl apply -f -
 
 kubectl get secret argocd-initial-admin-secret -n argocd -o jsonpath='{.data.password}' | base64 -d
-
 kubectl port-forward svc/argocd-server -n argocd 8080:443
 
 kubectl get secret argocd-tls-cert -n argocd -o jsonpath='{.data.tls\.crt}' | base64 -d > argocd.crt
