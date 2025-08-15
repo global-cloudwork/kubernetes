083a15e2772eb7d120883a16041646ec56cf6bdd
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-15 11:38:53 -0400

    test

--- scripts/remove-and-init.sh
+++ scripts/remove-and-init.sh
@@ -14,20 +14,22 @@ systemctl start rke2-server.service
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
 chmod 644 /etc/rancher/rke2/rke2.yaml
 
-
-
+#Apply Remote
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.3.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
 
+#Apply From Repo
 kubectl create namespace argocd
 kubectl create namespace cert-manager
-kubectl create secret tls ca -n default --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
 
-openssl genrsa -out argocd-key.pem 2048
-openssl req -new -x509 -key argocd-key.pem -out argocd.localhost.pem -days 365 \
-  -subj "/CN=argocd.localhost/O=argocd"
-kubectl create secret tls argocd-tls \
-  --cert=argocd.localhost.pem \
-  --key=argocd-key.pem \
-  -n argocd
+
+# kubectl create secret tls ca -n default --key=../../keys/argocd-key.pem --cert=../../keys/argocd.localhost.pem
+
+# openssl genrsa -out argocd-key.pem 2048
+# openssl req -new -x509 -key argocd-key.pem -out argocd.localhost.pem -days 365 \
+#   -subj "/CN=argocd.localhost/O=argocd"
+# kubectl create secret tls argocd-tls \
+#   --cert=argocd.localhost.pem \
+#   --key=argocd-key.pem \
+#   -n argocd
 
