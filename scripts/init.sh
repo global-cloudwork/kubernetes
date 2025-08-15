29a2987e9061949e710cf707ec3cdbb0c72c809d
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-15 14:09:35 -0400

    test

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,14 +2,19 @@
 ./remove-rke2.sh
 ./install-rke2.sh
 
-echo "Install Tools"
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes?ref=development" \
-  | kubectl apply -f -
+# ls -l /etc/rancher/rke2/rke2.yaml
+# ls -l "$HOME/.kube/config"
 
 # kubectl wait --for=condition=Ready pods --all --namespace my-namespace --timeout=300s
 
-echo "Install Core"
-kubectl kustomize --enable-helm \
-  "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
-  | kubectl apply -f -
+# echo "Install Tools"
+# kubectl kustomize --enable-helm \
+#   "github.com/global-cloudwork/kubernetes?ref=development" \
+#   | kubectl apply -f -
+
+# kubectl wait --for=condition=Ready pods --all --namespace my-namespace --timeout=300s
+
+# echo "Install Core"
+# kubectl kustomize --enable-helm \
+#   "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
+#   | kubectl apply -f -
--- scripts/install-rke2.sh
+++ scripts/install-rke2.sh
@@ -3,7 +3,9 @@ echo "Install RKE2"
 curl -sfL https://get.rke2.io | sudo sh -
 
 mkdir -p /etc/rancher/rke2/
-cp ../configurations/etc-rancher-rke2-config.yaml /etc/rancher/rke2/config.yaml
+mkdir -p $HOME/.kube/
+cp ../configurations/local-kubeconfig.yaml /etc/rancher/rke2/config.yaml
+cp ../configurations/local-kubeconfig.yaml $HOME/.kube/config.yaml
 
 systemctl enable rke2-server.service
 systemctl start rke2-server.service
