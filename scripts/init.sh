999fe25c74757704fd58f58111802d2548f34ed0
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-15 17:24:26 -0400

    test

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
-./remove-rke2.sh
+sudo ./remove-rke2.sh
 ./install-rke2.sh
-
-sudo chmod a+r /etc/rancher/rke2/rke2.yaml
 
 # ls -l /etc/rancher/rke2/rke2.yaml
 # ls -l "$HOME/.kube/"
 
-echo Installing CRD's
 kubectl kustomize --enable-helm \
   "github.com/global-cloudwork/kubernetes/tools?ref=development" \
   | kubectl apply -f -
 
-# echo Waiting For CRD's
-# kubectl wait --for=condition=Established crd --all --timeout=300s
-
-# echo Installing Cilium
-echo "Install Cilium"
 kubectl kustomize --enable-helm \
   "github.com/global-cloudwork/kubernetes/applications/core/cilium?ref=development" \
   | kubectl apply -f -
 
-# kubectl wait --for=condition=Ready pods --all --namespace my-namespace --timeout=300s
+kubectl kustomize --enable-helm \
+  "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
+  | kubectl apply -f -
 
-# echo "Install Core"
-# kubectl kustomize --enable-helm \
-#   "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
-#   | kubectl apply -f -
+# sudo chmod a+r /etc/rancher/rke2/rke2.yaml
