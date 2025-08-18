dc1357c57bb6d3a610f3915437b453959868f933
mcconnellj <josh.v.mcconnell@gmail.com>
2025-08-18 17:42:33 -0400

    diferent machine.

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,9 +1,9 @@
 kubectl apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.3.0/standard-install.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/gateway-api/v1.3.0/config/crd/experimental/gateway.networking.k8s.io_tlsroutes.yaml
 
-# kubectl kustomize --enable-helm \
-#   "github.com/global-cloudwork/kubernetes/tools?ref=development" \
-#   | kubectl apply -f -
+kubectl kustomize --enable-helm \
+  "github.com/global-cloudwork/kubernetes/tools?ref=development" \
+  | kubectl apply -f -
 
 # kubectl kustomize --enable-helm \
 #   "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=development" \
--- scripts/install-rke2.sh
+++ scripts/install-rke2.sh
@@ -5,5 +5,14 @@ curl -sfL https://get.rke2.io | sudo sh -
 mkdir -p /etc/rancher/rke2/
 cp ../configurations/local-kubeconfig.yaml /etc/rancher/rke2/config.yaml
 
+echo -e "\nnode-ip: \"$(hostname -I | awk '{print $1}')\"" | sudo tee -a /etc/rancher/rke2/config.yaml
+
 systemctl enable rke2-server.service
-# systemctl start rke2-server.service
+systemctl start rke2-server.service
+
+if ! echo "$PATH" | grep -q "/var/lib/rancher/rke2/bin"; then
+  echo "export PATH=\$PATH:/var/lib/rancher/rke2/bin" >> ~/.profile
+  echo "Added /var/lib/rancher/rke2/bin to PATH in ~/.profile"
+else
+  echo "/var/lib/rancher/rke2/bin is already in PATH"
+fi
