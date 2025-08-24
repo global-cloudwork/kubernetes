49082721462d5d0fdd698230b820d289126dd204
josh mcconnell <josh.v.mcconnell@gmail.com>
2025-08-24 10:34:33 -0400

    test

--- scripts/install-and-bootstrap.sh
+++ scripts/install-and-bootstrap.sh
@@ -22,7 +22,6 @@ fi
 export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
 chmod a+r /etc/rancher/rke2/rke2.yaml
 
-#!/bin/bash
 
 # Wait for all nodes to be ready
 /var/lib/rancher/rke2/bin/kubectl wait --for=condition=Ready nodes --all --timeout=300s
@@ -34,4 +33,4 @@ kubectl kustomize "github.com/global-cloudwork/kubernetes/kubernetes/bootstrap?r
 kubectl wait --for=condition=Ready nodes --all --timeout=300s
 
 # Apply the ArgoCD core application
-kubectl kustomize "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=main" | kubectl apply --enable-helm --server-side --force-conflicts -f -
+kubectl kustomize --enable-helm "github.com/global-cloudwork/kubernetes/applications/core/argocd?ref=main" | kubectl apply --server-side --force-conflicts -f -
