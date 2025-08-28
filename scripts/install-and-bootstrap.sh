067c92844fbe79284a5787d6009f571d0a9c067a
josh m <josh.v.mcconnell@gmail.com>
2025-08-28 12:11:43 -0400

    test

--- scripts/install-and-bootstrap.sh
+++ scripts/install-and-bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
 echo Script Start - Configure a new RKE2 instilation, deploy manifests
 
 echo curl and run installer script https://get.rke2.io
@@ -30,13 +32,13 @@ export KUBECONFIG=/etc/rancher/rke2/rke2.yaml
 chmod a+r /etc/rancher/rke2/rke2.yaml
 
 echo waiting for the node, then all of its pods
-/var/lib/rancher/rke2/bin/kubectl wait --for=condition=Ready node --all --timeout=600s
+kubectl wait --for=condition=Ready node --all --timeout=600s
 
 echo applying crds and other manifests /kubernetes/bootstrap 
-/var/lib/rancher/rke2/bin/kubectl kustomize "github.com/global-cloudwork/kubernetes/kubernetes/bootstrap?ref=main" | kubectl apply --wait --server-side --force-conflicts -f -
+kubectl kustomize "github.com/global-cloudwork/kubernetes/kubernetes/bootstrap?ref=main" | kubectl apply --wait --server-side --force-conflicts -f -
 
 echo applying the argocd helm chart, turned manifest /applications/argocd
-/var/lib/rancher/rke2/bin/kubectl kustomize --enable-helm "github.com/global-cloudwork/kubernetes/applications/argocd?ref=main" | kubectl apply --wait --server-side --force-conflicts -f -
+kubectl kustomize --enable-helm "github.com/global-cloudwork/kubernetes/applications/argocd?ref=main" | kubectl apply --wait --server-side --force-conflicts -f -
 
 echo applying the development kustomize overlay environments/development
-/var/lib/rancher/rke2/bin/kubectl kustomize --enable-helm "github.com/global-cloudwork/kubernetes/environments/development?ref=main" | kubectl apply --server-side --force-conflicts -f -
+kubectl kustomize --enable-helm "github.com/global-cloudwork/kubernetes/environments/development?ref=main" | kubectl apply --server-side --force-conflicts -f -
