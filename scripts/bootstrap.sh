8200c57618466a3c80da201caa5fb977ba9d8acd
josh m <josh.v.mcconnell@gmail.com>
2025-08-31 13:19:23 -0400

    test

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+CLUSTER_NAME=on-site
+
 REVISION=main
 REPOSITORY=global-cloudwork/kubernetes
 RAW_REPOSITORY=https://raw.githubusercontent.com/$REPOSITORY/$REVISION
 
-CLUSTER_CONFIG_PATH=$RAW_REPOSITORY/configurations/on-site.yaml
-HELM_CONFIG_PATH=$RAW_REPOSITORY/configurations/helm-chart-config.crd.yaml
+RKE2_CONFIG_PATH=$RAW_REPOSITORY/configurations/clusters/$CLUSTER_NAME/rke2-configuration.yaml
+CILIUM_CONFIG_PATH=$RAW_REPOSITORY/configurations/clusters/$CLUSTER_NAME/cilium-configuration.crd.yaml
 
 declare -a KUSTOMIZE_PATHS=(
 "components/bootstrap"
@@ -37,12 +39,12 @@ mkdir -p /etc/rancher/rke2/
 mkdir -p /var/lib/rancher/rke2/server/manifests
 
 echo "Curl cluster config, and helm chart config"
-sudo curl -o /etc/rancher/rke2/config.yaml $CLUSTER_CONFIG_PATH
-sudo curl -o /var/lib/rancher/rke2/server/manifests/helm-chart-config.crd.yaml $HELM_CONFIG_PATH
+sudo curl -o /etc/rancher/rke2/config.yaml $RKE2_CONFIG_PATH
+sudo curl -o /var/lib/rancher/rke2/server/manifests/helm-chart-config.crd.yaml $CILIUM_CONFIG_PATH
 
 echo "Modify configurations to add hostname"
 sudo echo -e '\ntls-san:\n  - $(hostname -f)' >> /etc/rancher/rke2/config.yaml
-sudo echo -e '\nnode-name: '$NODE_NAME >> /etc/rancher/rke2/config.yaml
+sudo echo -e '\nnode-name: '$CLUSTER_NAME >> /etc/rancher/rke2/config.yaml
 
 echo "Enable, then start the rke2-server service"
 systemctl enable --now rke2-server.service
