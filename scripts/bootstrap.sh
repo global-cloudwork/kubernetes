3bc033d0a0f4960de13edf87c4129d0cd8933e1a
josh m <josh.v.mcconnell@gmail.com>
2025-09-07 12:22:40 -0400

    test

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
+# sudo apt-get update -y
+# sudo apt-get install -y open-iscsi nfs-common util-linux dmsetup cryptsetup
+# sudo systemctl enable --now iscsid
+# sudo modprobe iscsi_tcp
+# sudo modprobe dm_crypt
+# echo dm_crypt | sudo tee /etc/modules-load.d/longhorn.conf
 
 CLUSTER_NAME=on-site
 
