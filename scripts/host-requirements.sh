3bee6a3bc445d0c586e7fd31753bf7cf40ca74f9
josh m <josh.v.mcconnell@gmail.com>
2025-09-09 12:34:59 -0400

    test

---
+++ scripts/host-requirements.sh
@@ -0,0 +1,14 @@
+apt update && apt upgrade -y
+apt install -y curl tree
+
+#gcloud cli
+cd ~
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
+
+sudo apt install git -y
+curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+  git config --global user.email "josh.v.mcconnell@gmail.com"
+  git config --global user.name "josh m"
