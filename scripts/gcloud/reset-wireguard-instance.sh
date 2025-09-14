e2bd1984f8a8be0dab1d6903a0113c64af792156
josh m <josh.v.mcconnell@gmail.com>
2025-09-14 11:40:26 -0400

    test

--- scripts/gcloud/reset-wireguard-instance.sh
+++ scripts/gcloud/reset-wireguard-instance.sh
@@ -17,6 +17,7 @@ INSTANCE_NAME=wireguard
 GCP_ZONE=us-central1-a
 MACHINE_TYPE=e2-micro
 STARTUP_SCRIPT_URL=https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/scripts/rke2/install-server.sh
+IMAGE=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2504-plucky-amd64-v20250911
 
 h2 "apt installing curl, helm, kubectl"
 sudo apt-get install -y curl wireguard
@@ -36,16 +37,19 @@ PUBLIC_KEY=$(echo "$PUBLIC_KEY" | base64 -w0)
 ALLOWED_IPS=$(echo "$ALLOWED_IPS" | base64 -w0)
 
 h1 "Creating Compute Instance $INSTANCE_NAME in Project $GCP_PROJECT"
-echo "checking if instance exists..."
+
+h2 "checking if instance exists..."
 if [ -n "$(gcloud compute instances list --filter="name:($INSTANCE_NAME)" --format="value(name)" --project="$GCP_PROJECT" --zones="$GCP_ZONE")" ]; then
     echo "it exists, deleting"
     gcloud compute instances delete "$INSTANCE_NAME" --project="$GCP_PROJECT" --zone="$GCP_ZONE" --quiet
 fi
 
+h2 "Waiting for instance to delete"
 while [[ $(gcloud compute instances describe "$INSTANCE_NAME" --project="$GCP_PROJECT" --zone="$GCP_ZONE" &> /dev/null; echo $?) -eq 0 ]]; do
     sleep 5
 done
 
+h2 "Creating instance $INSTANCE_NAME"
 gcloud compute instances create "$INSTANCE_NAME" \
     --project="$GCP_PROJECT" \
     --zone="$GCP_ZONE" \
@@ -56,7 +60,7 @@ gcloud compute instances create "$INSTANCE_NAME" \
     --service-account="$SERVICE_ACCOUNT" \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
     --tags=http-server,https-server \
-    --create-disk=auto-delete=yes,boot=yes,device-name=wireguard,image=projects/ubuntu-os-cloud/global/images/ubuntu-minimal-2504-plucky-amd64-v20250828,mode=rw,size=10,type=pd-standard \
+    --create-disk=auto-delete=yes,boot=yes,device-name=wireguard,image="$IMAGE",mode=rw,size=10,type=pd-standard \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
@@ -64,6 +68,7 @@ gcloud compute instances create "$INSTANCE_NAME" \
     --reservation-affinity=any \
     --metadata=startup-script-url="$STARTUP_SCRIPT_URL",CILIUM-CA="$CILIUM_CA",PUBLIC-KEY="$PUBLIC_KEY",ALLOWED-IPS="$ALLOWED_IPS"
 
+h2 "Waiting for instance to be running"
 while true; do
     STATUS=$(gcloud compute instances describe "$INSTANCE_NAME" --project="$GCP_PROJECT" --zone="$GCP_ZONE" --format='get(status)')
     if [[ "$STATUS" == "RUNNING" ]]; then
@@ -74,15 +79,15 @@ while true; do
     sleep 5
 done
 
-echo "Streaming startup script output..."
-gcloud compute instances tail-serial-port-output "$INSTANCE_NAME" \
-    --project="$GCP_PROJECT" \
-    --zone="$GCP_ZONE" | grep "Startup script finished"
-
 gcloud compute ssh ubuntu@$INSTANCE_NAME --project=$GCP_PROJECT --zone=$GCP_ZONE
 
-echo "Endlessly watching serial output for 'STARTUP COMPLETE' message"
-gcloud compute instances get-serial-port-output $INSTANCE_NAME --zone=$GCP_ZONE | grep "STARTUP COMPLETE"
+# h2 "Streaming startup script output..."
+# gcloud compute instances tail-serial-port-output "$INSTANCE_NAME" \
+#     --project="$GCP_PROJECT" \
+#     --zone="$GCP_ZONE" | grep "Startup script finished"
+
+# h2 "Endlessly watching serial output for 'STARTUP COMPLETE' message"
+# gcloud compute instances get-serial-port-output $INSTANCE_NAME --zone=$GCP_ZONE | grep "STARTUP COMPLETE"
 
 gcloud compute instances remove-metadata INSTANCE_NAME \
     --zone=ZONE_NAME \
