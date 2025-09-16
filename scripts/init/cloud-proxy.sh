6382efe6cdd43b300dfc063cb4cf8da7c577d27a
josh m <josh.v.mcconnell@gmail.com>
2025-09-15 21:02:47 -0400

    test

--- scripts/init/cloud-proxy.sh
+++ scripts/init/cloud-proxy.sh
@@ -10,13 +10,13 @@ RAW_REPOSITORY=https://raw.githubusercontent.com/$REPOSITORY/$REVISION
 
 # Values passed to the startup script using encrypted metadata
 AUTHORS_PUBLIC_KEY=$(curl -s -H "Metadata-Flavor: Google" \
-    http://metadata.google.internal/computeMetadata/v1/instance/attributes/authors-public-key | base64 -d)
+    http://metadata.google.internal/computeMetadata/v1/instance/authors-public-key | base64 -d)
 AUTHORS_IP=$(curl -s -H "Metadata-Flavor: Google" \
-    http://metadata.google.internal/computeMetadata/v1/instance/attributes/allowed-ip | base64 -d)
+    http://metadata.google.internal/computeMetadata/v1/instance/allowed-ip | base64 -d)
 CILIUM_CA=$(curl -s -H "Metadata-Flavor: Google" \
-    http://metadata.google.internal/computeMetadata/v1/instance/attributes/cilium-ca | base64 -d)
+    http://metadata.google.internal/computeMetadata/v1/instance/cilium-ca | base64 -d)
 ADDRESS=$(curl -s -H "Metadata-Flavor: Google" \
-    http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip | base64 -d)
+    http://metadata.google.internal/computeMetadata/v1/instance/external-ip | base64 -d)
 
 echo "AUTHORS_PUBLIC_KEY: $AUTHORS_PUBLIC_KEY"
 echo "AUTHORS_IP: $AUTHORS_IP"
