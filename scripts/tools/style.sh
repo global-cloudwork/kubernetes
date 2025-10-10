33f92fcc5f89f7b02729efb0faf3de81918c44c5
josh m <josh.v.mcconnell@gmail.com>
2025-10-10 16:21:59 -0400

    test

---
+++ scripts/tools/style.sh
@@ -0,0 +1,9 @@
+# ======================== Style Starts Here ========================
+
+function h1() {
+  command echo -e "\n\033[4m\033[38;5;11m# $1\033[0m"
+}
+
+function h2() {
+    command echo -e "\n\033[4m\033[38;5;9m## $1\033[0m"
+}
