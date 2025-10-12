137b79f76c19877271a730df9931ab75280eddd2
josh m <josh.v.mcconnell@gmail.com>
2025-10-12 15:16:24 -0400

    test

--- scripts/cloud-proxy/init.sh
+++ scripts/cloud-proxy/init.sh
@@ -11,11 +11,11 @@ ITALIC="\e[3m"
 UNDERLINE="\e[4m"
 RESET="\e[0m"
 
-title()   { printf "\n${BOLD}${UNDERLINE}\e[38;5;231m%s${RESET}\n" "$1"; }
-section() { printf "\n${BOLD}${UNDERLINE}\e[38;5;51m%s${RESET}\n" "$1"; }
-header()  { printf "\n${ITALIC}\e[38;5;33m%s${RESET}\n" "$1"; }
-error()   { printf "\n${BOLD}${ITALIC}${UNDERLINE}\e[38;5;106m%s${RESET}\n" "$1"; }
-note()    { printf "\n${BOLD}${ITALIC}\e[38;5;82m%s${RESET}\n" "$1"; }
+title()   { printf "\n${BOLD}${UNDERLINE}\e[38;5;231m%s${RESET}\n\n" "$1"; }
+section() { printf "\n${BOLD}${UNDERLINE}\e[38;5;51m%s${RESET}\n\n" "$1"; }
+header()  { printf "\n${ITALIC}\e[38;5;33m%s${RESET}\n\n" "$1"; }
+error()   { printf "\n${BOLD}${ITALIC}${UNDERLINE}\e[38;5;106m%s${RESET}\n\n" "$1"; }
+note()    { printf "\n${BOLD}${ITALIC}\e[38;5;82m%s${RESET}\n\n" "$1"; }
 
 title "Configure RKE2 & Deploy Kustomizations"
 
