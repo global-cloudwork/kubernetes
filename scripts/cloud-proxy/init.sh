3e88367a264db7c23044a0c90fec1223bb017b0f
josh m <josh.v.mcconnell@gmail.com>
2025-10-12 15:04:46 -0400

    test

--- scripts/cloud-proxy/init.sh
+++ scripts/cloud-proxy/init.sh
@@ -6,9 +6,9 @@
 #sudo journalctl -u google-startup-scripts.service --no-pager
 #sudo systemctl status rke2-server.service
 
-title()   { printf "\n\033[1;4;38;5;231m# %s\033[0m\n\n" "$1"; }   # Bright white
-section() { printf "\n\033[1;38;5;51m# %s\033[0m\n\n" "$1"; }       # Cyan
-header()  { printf "\n\033[1;3;38;5;33m## %s\033[0m\n\n" "$1"; }    # Blue
+title()   { printf "\n\033[1;4;38;5;231m# %s\033[0m\n\n\n\n" "$1"; }   # Bright white
+section() { printf "\n\033[1;38;5;51m# %s\033[0m\n\n\n\n" "$1"; }       # Cyan
+header()  { printf "\n\033[1;3;38;5;33m## %s\033[0m\n\n\n\n" "$1"; }    # Blue
 error()   { printf "\n\033[1;4;38;5;196mError:\033[0m \033[1m%s\033[0m\n\n" "$1"; }  # Bright red
 note()    { printf "\n\033[1;3;38;5;82mNote:\033[0m \033[1m%s\033[0m\n\n" "$1"; }   # Bright green
 
