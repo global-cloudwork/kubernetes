d8175a76420b883939b32f7e17c689e272bf5011
josh m <josh.v.mcconnell@gmail.com>
2025-10-19 21:00:20 -0400

    test

--- scripts/remove.sh
+++ scripts/remove.sh
@@ -1,32 +1,35 @@
 #!/bin/bash
 #Everything Is Supressed
 
+source <(curl -sSL https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/scripts/general.sh)
+
+
 echo() {
     command echo -e "\n\033[4m\033[38;5;9m## $1\033[0m"
 }
 
-function h1() {
+function title() {
   command echo -e "\n\033[4m\033[38;5;11m# $1\033[0m"
 }
 
-function h2() {
+function header() {
     command echo -e "\n\033[4m\033[38;5;9m## $1\033[0m"
 }
 
-h1 "Removing Existing RKE2 Resources"
+title "Removing Existing RKE2 Resources"
 
-h2 "remove symbolic links before re-installing"
+header "remove symbolic links before re-installing"
 rm $HOME/.kube/
 sudo rm /usr/local/bin/kubectl
 
-h2 "rke2 scripts, kill all and uninstall"
+header "rke2 scripts, kill all and uninstall"
 sudo /usr/local/bin/rke2-killall.sh
 sudo /usr/local/bin/rke2-uninstall.sh
 
-h2 "System agent uninstall script"
+header "System agent uninstall script"
 curl -sS https://raw.githubusercontent.com/rancher/system-agent/main/system-agent-uninstall.sh | sudo sh &>/dev/null
 
-h2 "Folder removal"
+header "Folder removal"
 rm -rf /etc/ceph \
        /etc/cni \
        /etc/kubernetes \
@@ -45,7 +48,7 @@ rm -rf /etc/ceph \
        /var/log/pods \
        /var/run/calico &>/dev/null
 
-h2 "iptables removal (optional but good for a fresh start)"
+header "iptables removal (optional but good for a fresh start)"
 sudo iptables -t nat -X FLANNEL-POSTRTG &>/dev/null
 sudo iptables -t nat -F &>/dev/null # Optional: Also flush the NAT table entirely
 
