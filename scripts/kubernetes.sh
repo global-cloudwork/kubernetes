e78321b5287844bd3777f17df6f7bb1a8e4ea3e2
josh m <josh.v.mcconnell@gmail.com>
2025-10-21 13:46:55 -0400

    test

--- scripts/kubernetes.sh
+++ scripts/kubernetes.sh
@@ -18,7 +18,7 @@ unfinished_crds() {
 wait_for() {
     local TYPE="$1"
     local SECONDS_TO_WAIT="${SECONDS_TO_WAIT:-10}"
-    local MAX_ITERATIONS="${MAX_ITERATIONS:-20}"
+    local MAX_ITERATIONS="${MAX_ITERATIONS:-12}"
 
     local FUNC_NAME="unfinished_$TYPE"
 
