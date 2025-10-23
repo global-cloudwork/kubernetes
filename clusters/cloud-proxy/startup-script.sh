2f3196d547c46491b1cff1336b8285c4147cabe4
josh m <josh.v.mcconnell@gmail.com>
2025-10-23 12:33:47 -0400

    test

--- clusters/cloud-proxy/startup-script.sh
+++ clusters/cloud-proxy/startup-script.sh
@@ -4,4 +4,4 @@ apt upgrade
 
 mkdir /home/ubuntu/init.sh
 sudo curl --silent --show-error -o /usr/local/bin/bootstrap.sh https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/clusters/cloud-proxy/init.sh
-sudo chmod 755 /usr/local/bin/bootstrap.sh
+sudo chmod 755 /usr/local/bin/init
