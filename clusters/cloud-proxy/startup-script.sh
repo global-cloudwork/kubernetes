793df27db69d6c01998defe1a7466d8326d605ed
josh m <josh.v.mcconnell@gmail.com>
2025-10-23 12:09:45 -0400

    test

--- clusters/cloud-proxy/startup-script.sh
+++ clusters/cloud-proxy/startup-script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-# Runs as root on first boot
-
-# 1. Update system packages
 apt update -y
 apt upgrade
 
 mkdir /home/ubuntu/init.sh
 sudo curl --silent --show-error -o /usr/local/bin/init.sh https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/clusters/cloud-proxy/init.sh
-sudo chmod 755 /home/ubuntu/init.sh
+sudo chmod 755 /usr/local/bin/init.sh
