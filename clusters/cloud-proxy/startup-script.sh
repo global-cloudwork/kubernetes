d2cfccf2c4919ffa8deacd4b7ed4ba4fbf5812e5
josh m <josh.v.mcconnell@gmail.com>
2025-10-23 11:54:41 -0400

    test

--- clusters/cloud-proxy/startup-script.sh
+++ clusters/cloud-proxy/startup-script.sh
@@ -6,5 +6,5 @@ apt update -y
 apt upgrade
 
 mkdir /home/ubuntu/init.sh
-curl --silent --show-error -o /home/ubuntu/init.sh https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/clusters/cloud-proxy/init.sh
-chmod 755 /home/ubuntu/init.sh
+sudo curl --silent --show-error -o /usr/local/bin/init.sh https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/clusters/cloud-proxy/init.sh
+sudo chmod 755 /home/ubuntu/init.sh
