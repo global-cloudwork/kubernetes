14464d5f87550df54d15bf8f03d2cef3260bfe21
josh m <josh.v.mcconnell@gmail.com>
2025-10-24 23:27:20 -0400

    test

--- clusters/cloud-proxy/init.sh
+++ clusters/cloud-proxy/init.sh
@@ -166,24 +166,24 @@ sudo chown "$USER":"$USER" "$HOME/.kube/$CLUSTER_NAME/config"
 KUBECONFIG_LIST=$(find -L /home/ubuntu/.kube -mindepth 2 -type f -name config | paste -sd:)
 sudo kubectl --kubeconfig="$KUBECONFIG_LIST" config view --flatten | sudo tee /home/ubuntu/.kube/config > /dev/null
 
-# wait_for pods
+wait_for pods
 
-# section "Deploy pre-start manifests"
-# header "Applying Kustomize PATH: base/core"
-# kubectl kustomize --enable-helm "github.com/$REPOSITORY/base/core?ref=$BRANCH" | \
-#   kubectl apply --server-side --force-conflicts -f -
+section "Deploy pre-start manifests"
+header "Applying Kustomize PATH: base/core"
+kubectl kustomize --enable-helm "github.com/$REPOSITORY/base/core?ref=$BRANCH" | \
+  kubectl apply --server-side --force-conflicts -f -
 
-# section "Deploy argocd manifests"
-# header "Applying Kustomize PATH: applications/argocd"
-# kubectl kustomize --enable-helm "github.com/$REPOSITORY/applications/argocd?ref=$BRANCH" | \
-#   kubectl apply --server-side --force-conflicts -f -
+section "Deploy argocd manifests"
+header "Applying Kustomize PATH: applications/argocd"
+kubectl kustomize --enable-helm "github.com/$REPOSITORY/applications/argocd?ref=$BRANCH" | \
+  kubectl apply --server-side --force-conflicts -f -
 
-# header "Deploy cert-manager manifests"
-# kubectl kustomize --enable-helm "github.com/$REPOSITORY/applications/cert-manager?ref=$BRANCH" | \
-#   kubectl apply --server-side --force-conflicts -f -
+header "Deploy cert-manager manifests"
+kubectl kustomize --enable-helm "github.com/$REPOSITORY/applications/cert-manager?ref=$BRANCH" | \
+  kubectl apply --server-side --force-conflicts -f -
 
-# wait_for endpoints
+wait_for endpoints
 
-# header "Deploy startup manifests"
-# kubectl kustomize --enable-helm "github.com/$REPOSITORY/base?ref=$BRANCH" | \
-#   kubectl apply --server-side --force-conflicts -f -
+header "Deploy startup manifests"
+kubectl kustomize --enable-helm "github.com/$REPOSITORY/base?ref=$BRANCH" | \
+  kubectl apply --server-side --force-conflicts -f -
