9dbed61b3dd4c5b5258f35f426fa286512511716
josh m <josh.v.mcconnell@gmail.com>
2025-10-31 21:05:42 -0400

    test

---
+++ clusters/base/init.sh
@@ -0,0 +1,125 @@
+#!/usr/bin/env bash
+
+# for pod in $(kubectl get pods -A -o custom-columns=NAMESPACE:.metadata.namespace,NAME:.metadata.name --no-headers | sed 's/  */,/g'); do
+#   NAMESPACE=$(echo $pod | cut -d',' -f1)
+#   NAME=$(echo $pod | cut -d',' -f2)
+#   echo "--- Checking logs for $NAMESPACE/$NAME ---"
+#   kubectl logs -n $NAMESPACE $NAME --tail=500 2>/dev/null | grep -i error
+#   echo
+# done
+
+
+# kubectl get pods -A -o custom-columns=:.metadata.name --no-headers | xargs -I {} kubectl logs -n argocd {} --tail=500 | grep -i error
+# curl --silent --show-error https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/clusters/cloud-proxy/init.sh | bash
+#
+#sudo journalctl -u google-startup-scripts.service --no-pager
+#sudo systemctl status rke2-server.service
+#sudo journalctl -u rke2-server -f
+#sudo ss -tulnp
+
+#===============================================================================
+# Main Script Entry Point
+#
+#This script has a few sections:
+#
+#
+#===============================================================================
+
+# Import environment variables from Secret Manager, instance metadata, and bash
+export $(gcloud secrets versions access latest --secret=development-env-file | xargs)
+export EXTERNAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+export INTERNAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip)
+source <(curl -sSL https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/scripts/general.sh)
+source <(curl -sSL https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/scripts/kubernetes.sh)
+
+# Set PATH to include rke2 binaries
+export PATH=/var/lib/rancher/rke2/bin:$PATH
+PATH=$PATH:/opt/rke2/bin
+
+
+
+#===============================================================================
+# Prepare the host system
+#===============================================================================
+section "Prepare the host system"
+#===============================================================================
+
+# Install required system packages and create necessary directories
+header "apt-get update & install"
+sudo apt-get -qq update
+sudo apt-get -qq -y install git wireguard
+mkdir -p $HOME/.kube/$CLUSTER_NAME
+sudo mkdir -p /etc/rancher/rke2/
+sudo mkdir -p /var/lib/rancher/rke2/server/manifests/
+
+# Download RKE2 configuration files, then substitute environment variables
+sudo curl --silent --show-error --remote-name-all --output-dir /tmp/ \
+  https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/clusters/cloud-proxy/configurations/config.yaml
+sudo --preserve-env envsubst < /tmp/config.yaml | sudo tee /etc/rancher/rke2/config.yaml
+
+# Install Helm and RKE2
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 \
+    --remote-name-all --silent --show-error | bash
+curl https://get.rke2.io \
+  --remote-name-all --silent --show-error | sudo bash
+
+
+
+#===============================================================================
+# Configure and start the RKE2 service
+#===============================================================================
+section "Configure and start the RKE2 service"
+#===============================================================================
+
+# Enable on boot, then start of RKE2
+header "First start of RKE2 to install crd's"
+sudo systemctl enable rke2-server.service
+sudo systemctl start rke2-server.service
+
+# Link kubectl command avoiding race conditions
+sudo ln -s /var/lib/rancher/rke2/bin/kubectl /usr/local/bin/kubectl
+
+# Copy RKE2-generated kubeconfig, set proper ownership, and merge all kubeconfig files
+sudo cp -f /etc/rancher/rke2/rke2.yaml $HOME/.kube/$CLUSTER_NAME/config
+sudo chown "$USER":"$USER" "$HOME/.kube/$CLUSTER_NAME/config"
+KUBECONFIG_LIST=$(find -L /home/ubuntu/.kube -mindepth 2 -type f -name config | paste -sd:)
+sudo kubectl --kubeconfig="$KUBECONFIG_LIST" config view --flatten | sudo tee /home/ubuntu/.kube/config > /dev/null
+
+
+
+#===============================================================================
+# Deploy Base, Core, and ArgoCD Bootstrap
+#===============================================================================
+section "Deploy CRD's, Cilium, and ArgoCD Bootstrap"
+#===============================================================================
+
+# Deploy Base
+header "Applying Kustomize PATH: base"
+kubectl kustomize --enable-helm "github.com/$REPOSITORY/base?ref=$BRANCH" | \
+  kubectl apply --server-side --force-conflicts -f -
+
+wait_for crds
+
+# Deploy Core
+header "Applying Kustomize PATH: base/core"
+kubectl kustomize --enable-helm "github.com/$REPOSITORY/base/core?ref=$BRANCH" | \
+  kubectl apply --server-side --force-conflicts -f -
+
+# Restart RKE2 to pick up new manifests
+header "Restart RKE2 to pick up new manifests"
+sudo systemctl restart rke2-server.service
+
+# Copy RKE2-generated kubeconfig, set proper ownership, and merge all kubeconfig files
+sudo cp -f /etc/rancher/rke2/rke2.yaml /home/ubuntu/.kube/cloud-proxy/config
+sudo chown "$USER":"$USER" "$HOME/.kube/$CLUSTER_NAME/config"
+KUBECONFIG_LIST=$(find -L /home/ubuntu/.kube -mindepth 2 -type f -name config | paste -sd:)
+sudo kubectl --kubeconfig="$KUBECONFIG_LIST" config view --flatten | sudo tee /home/ubuntu/.kube/config > /dev/null
+
+# Create dns challenge key
+gcloud secrets versions access latest \
+  --secret="dns-solver-json-key" \
+  --project="global-cloudworks" \
+  > dns-key.json
+
+kubectl create secret generic dns-key --from-file=dns-key.json
+rm dns-key.json
