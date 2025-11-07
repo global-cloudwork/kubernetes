ba72ad49c751ec0b425703717d2645948a93ae89
josh m <josh.v.mcconnell@gmail.com>
2025-11-07 16:44:21 -0500

    test

--- tests/ingress.sh
+++ tests/ingress.sh
@@ -10,3 +10,10 @@ kubectl get gateway -A -o jsonpath='{range .items[*]}{.metadata.namespace}/{.met
 kubectl get httproute -A -o jsonpath='{range .items[*]}{.metadata.namespace}/{.metadata.name}{"\t"}{.status.parents[*].conditions[?(@.type=="Programmed")].status}{"\n"}{end}'
 kubectl get clusterissuer -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}'
 
+kubectl run dns-test --image=busybox:latest --restart=Never --rm -it -- sh
+
+kubectl get pod cert-manager-794db7f658-cfhbj -n cert-manager -o wide
+
+kubectl run connectivity-test -it --rm --image=curlimages/curl --restart=Never \
+--overrides='{"spec": {"nodeSelector": {"kubernetes.io/hostname": "cloud-proxy"}}}' \
+-- /bin/sh
