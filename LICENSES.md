af09fdcb0fb570876391aa68446533408b2c1a6d
josh m <josh.v.mcconnell@gmail.com>
2025-11-08 13:33:41 -0500

    test

--- LICENSES.md
+++ LICENSES.md
@@ -1,17 +1,9 @@
 This project claims the Apache License 2.0, where permissible.
 
 # Licenses
-
 - Cilium
 - Argo CD
-- RKE2 (confirmation needed)
+- RKE2
 - Cert-Manager
 
-All applications are used under the Apache License 2.0, which permits use, modification, and distribution.
-
-- Cilium
-- Argo CD
-- RKE2 (confirmation needed)
-- Cert-Manager
-
-All applications are used under the Apache License 2.0, which permits use, modification, and distribution.
+For the most part the applications used are intended to be opensource. 
--- scripts/tests/ingress.sh
+++ scripts/tests/ingress.sh
@@ -4,8 +4,6 @@ kubectl describe clusterissuer -A
 
 ubuntu@development:~$ kubectl get secrets -A | grep wildcard-certificate
 
-
-
 kubectl get gateway -A -o jsonpath='{range .items[*]}{.metadata.namespace}/{.metadata.name}{"\t"}{.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}'
 kubectl get httproute -A -o jsonpath='{range .items[*]}{.metadata.namespace}/{.metadata.name}{"\t"}{.status.parents[*].conditions[?(@.type=="Programmed")].status}{"\n"}{end}'
 kubectl get clusterissuer -o jsonpath='{range .items[*]}{.metadata.name}{"\t"}{.status.conditions[?(@.type=="Ready")].status}{"\n"}{end}'
