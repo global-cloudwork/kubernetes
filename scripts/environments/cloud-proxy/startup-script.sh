cbe58d2bc5eeeef37271f0c7b6c790f0cc6985a0
josh m <josh.v.mcconnell@gmail.com>
2025-11-09 16:07:43 -0500

    test

--- scripts/environments/cloud-proxy/startup-script.sh
+++ scripts/environments/cloud-proxy/startup-script.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-set -euo pipefail
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y curl
+apt update -y
+apt upgrade
 
-# Ensure compatibility for downstream scripts that call 'cURL'
-if [ ! -x /usr/bin/cURL ] && [ -x /usr/bin/curl ]; then
-	ln -sf /usr/bin/curl /usr/bin/cURL
-fi
+mkdir /home/ubuntu/init.sh
+sudo curl --silent --show-error -o /usr/local/bin/bootstrap https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/scripts/environments/cloud-proxy/startup-script.sh
+sudo chmod 755 /usr/local/bin/bootstrap
 
-curl --silent --show-error -o /usr/local/bin/bootstrap https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/scripts/environments/cloud-proxy/init-cloud-proxy.sh
-chmod 755 /usr/local/bin/bootstrap
+# Install K9s
+wget https://github.com/derailed/k9s/releases/latest/download/k9s_linux_amd64.deb && apt install ./k9s_linux_amd64.deb
+rm k9s_linux_amd64.deb
