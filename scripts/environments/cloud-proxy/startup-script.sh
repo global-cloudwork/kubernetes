cb26dfa01239c5276c566e58a0a3a504131dab01
josh m <josh.v.mcconnell@gmail.com>
2025-11-09 15:51:47 -0500

    test

--- scripts/environments/cloud-proxy/startup-script.sh
+++ scripts/environments/cloud-proxy/startup-script.sh
@@ -3,6 +3,5 @@ apt update -y
 apt upgrade
 apt install curl -y
 
-mkdir /home/ubuntu/init.sh
 sudo curl --silent --show-error -o /usr/local/bin/bootstrap https://raw.githubusercontent.com/global-cloudwork/kubernetes/main/scripts/environments/cloud-proxy/init-cloud-proxy.sh
 sudo chmod 755 /usr/local/bin/bootstrap
